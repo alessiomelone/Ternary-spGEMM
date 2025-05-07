603f824db22a55fda4134db09a9abf8ae72b1ec8
baraql <baraqlip@gmail.com>
2025-05-07 20:54:29 +0200

    compressed

---
+++ cpp_impl/data_structures/generate_map.py
@@ -0,0 +1,6 @@
+for i1 in range(-1, 2):
+    for i2 in range(-1, 2):
+        for i3 in range(-1, 2):
+            for i4 in range(-1, 2):
+                for i5 in range(-1, 2):
+                    print(f"$i1")
