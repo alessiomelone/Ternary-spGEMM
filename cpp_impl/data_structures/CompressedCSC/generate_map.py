8ca49b8b6bc8343c5792e8ef7d27e0f82d3b3c7d
baraql <baraqlip@gmail.com>
2025-05-14 16:14:11 +0200

    Implement CompressedCSC data structure with encoding and decoding functionality
    - Added CompressedCSC class for compressed storage of sparse matrices.
    - Implemented encoding scheme for matrix values using a bitstring representation.
    - Created a utility function to print matrices in a formatted manner.
    - Developed a Python script to generate mapping for encoding values.
    - Included necessary headers and ensured proper memory management.

--- cpp_impl/data_structures/CompressedCSC/generate_map.py
+++ cpp_impl/data_structures/CompressedCSC/generate_map.py
@@ -1,6 +1,8 @@
+i = 0
 for i1 in range(-1, 2):
     for i2 in range(-1, 2):
         for i3 in range(-1, 2):
             for i4 in range(-1, 2):
                 for i5 in range(-1, 2):
-                    print(f"$i1")
+                    print(f"{{{i1}, {i2}, {i3}, {i4}, {i5}}},")
+                    # i += 1
