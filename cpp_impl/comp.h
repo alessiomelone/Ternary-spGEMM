879ff53f2dcdd8579fe23fc15182d48ece97f265
Harry Maraziaris <harry@Harrys-MacBook-Air.local>
2025-05-24 13:00:03 +0200

    corrected B in readme

--- cpp_impl/comp.h
+++ cpp_impl/comp.h
@@ -7,11 +7,13 @@
 long long flops = 0;
 int ds_size = 0;
 
-long long getTotalFlops() {
+long long getTotalFlops()
+{
     return flops;
 }
 
-int getDataStructureSizeInBytes() {
+int getDataStructureSizeInBytes()
+{
     return ds_size;
 }
 #endif
@@ -91,21 +93,21 @@ void CSC_base_testing(T *X, const BaseTCSC &W_csr, T *b, T *Y, int M, int N, int
             T y_val = 0;
             for (int k = col_start_pos[n]; k < col_start_pos[n + 1]; k++)
             {
-            #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                 flops++;
-            #endif
+#endif
                 y_val += X[m * K + row_index_pos[k]];
             }
             for (int k = col_start_neg[n]; k < col_start_neg[n + 1]; k++)
             {
-            #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                 flops++;
-            #endif
+#endif
                 y_val -= X[m * K + row_index_neg[k]];
             }
-            #ifdef INSTRUMENTATION_RUN
-                flops++;
-            #endif
+#ifdef INSTRUMENTATION_RUN
+            flops++;
+#endif
             Y[m * N + n] = y_val + b[n];
         }
     }
@@ -127,66 +129,66 @@ void BaseCSC_unroll5(T *X, const BaseTCSC &W_csc,
 
     for (int m = 0; m < M; ++m)
     {
-        const T *x_row = X + m * K;                 // pointer to X[m,0]
+        const T *x_row = X + m * K; // pointer to X[m,0]
 
         for (int n = 0; n < N; ++n)
         {
             T y_val = 0;
 
             /* ---------- positive coefficients ---------- */
-            int k0   = col_start_pos[n];
+            int k0 = col_start_pos[n];
             int kEnd = col_start_pos[n + 1];
 
             /* main body: 5 nz per batch */
-            for ( ; k0 + 4 < kEnd; k0 += 5)
+            for (; k0 + 4 < kEnd; k0 += 5)
             {
-            #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                 flops += 5;
-            #endif
-                y_val += x_row[row_index_pos[k0    ]];
+#endif
+                y_val += x_row[row_index_pos[k0]];
                 y_val += x_row[row_index_pos[k0 + 1]];
                 y_val += x_row[row_index_pos[k0 + 2]];
                 y_val += x_row[row_index_pos[k0 + 3]];
                 y_val += x_row[row_index_pos[k0 + 4]];
             }
             /* tail */
-            for ( ; k0 < kEnd; ++k0) {
-            #ifdef INSTRUMENTATION_RUN
+            for (; k0 < kEnd; ++k0)
+            {
+#ifdef INSTRUMENTATION_RUN
                 flops++;
-            #endif
+#endif
                 y_val += x_row[row_index_pos[k0]];
             }
 
             /* ---------- negative coefficients ---------- */
-            k0   = col_start_neg[n];
+            k0 = col_start_neg[n];
             kEnd = col_start_neg[n + 1];
 
-            for ( ; k0 + 4 < kEnd; k0 += 5)
+            for (; k0 + 4 < kEnd; k0 += 5)
             {
-                y_val -= x_row[row_index_neg[k0    ]];
+                y_val -= x_row[row_index_neg[k0]];
                 y_val -= x_row[row_index_neg[k0 + 1]];
                 y_val -= x_row[row_index_neg[k0 + 2]];
-                y_val -= x_row[row_index_neg[k0 + 3]];
-                y_val -= x_row[row_index_neg[k0 + 4]];
-            #ifdef INSTRUMENTATION_RUN
+                y_val -= x_row[row_index_neg[k0 + 3]] y_val -= x_row[row_index_neg[k0 + 4]];
+#ifdef INSTRUMENTATION_RUN
                 flops += 5;
-            #endif
+#endif
             }
-            for ( ; k0 < kEnd; ++k0) {
+            for (; k0 < kEnd; ++k0)
+            {
                 y_val -= x_row[row_index_neg[k0]];
-            #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                 flops++;
-            #endif
+#endif
             }
             /* bias + store */
             Y[m * N + n] = y_val + b[n];
-            #ifdef INSTRUMENTATION_RUN
-                flops++;
-            #endif
+#ifdef INSTRUMENTATION_RUN
+            flops++;
+#endif
         }
     }
 }
-
 
 template <typename T>
 void CCSC_base(T *X, const CompressedCSC &W, T *b, T *Y, int M, int N, int K)
@@ -272,9 +274,9 @@ void CCSC_base(T *X, const CompressedCSC &W, T *b, T *Y, int M, int N, int K)
                 y_val2 += d[2] * X[m * K + row + 2];
                 y_val3 += d[3] * X[m * K + row + 3];
                 y_val4 += d[4] * X[m * K + row + 4];
-            #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                 flops += 5;
-            #endif
+#endif
             }
 
 <<<<<<< HEAD
@@ -285,6 +287,7 @@ void CCSC_base(T *X, const CompressedCSC &W, T *b, T *Y, int M, int N, int K)
 >>>>>>> 3e9bdeb (added run_benchmark.py)
             T acc = y_val0 + y_val1 + y_val2 + y_val3 + y_val4;
             Y[m * N + n] = acc + b[n];
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 >>>>>>> 00c090c (Add TCSR and TCSC base function prototypes; refactor CSC_base and CCSC_base implementations):cpp_impl/comp.cpp
@@ -302,6 +305,11 @@ void CCSC_base(T *X, const CompressedCSC &W, T *b, T *Y, int M, int N, int K)
             flops += 5;
         #endif
 >>>>>>> bc77f8b (Add Flop Instrumentation)
+=======
+#ifdef INSTRUMENTATION_RUN
+            flops += 5;
+#endif
+>>>>>>> da3265c (corrected B in readme)
         }
     }
 }
@@ -334,18 +342,20 @@ void TCSR(T *X, const CompressedTCSR &W, T *b, T *Y, int M, int N, int K)
             int row_start = row_offsets[k];
             int pos_count = row_pos_counts[k];
 
-            for (int i = row_start; i < row_start + pos_count; ++i) {
+            for (int i = row_start; i < row_start + pos_count; ++i)
+            {
                 Y_row[encoded_cols[i]] += x;
-            #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                 flops++;
-            #endif
+#endif
             }
 
-            for (int i = row_start + pos_count; i < row_offsets[k + 1]; ++i) {
+            for (int i = row_start + pos_count; i < row_offsets[k + 1]; ++i)
+            {
                 Y_row[-encoded_cols[i] - 1] -= x;
-            #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                 flops++;
-            #endif
+#endif
             }
         }
     }
@@ -365,7 +375,7 @@ void TCSR_unrolled_tiled(T *X, const CompressedTCSR &W, T *b, T *Y, int M, int N
     std::vector<T> y_row_accumulator_storage;
     if (N > 0)
     {
-        y_row_accumulator_storage.resize(N);  // TODO: Why is this not in the init code ?
+        y_row_accumulator_storage.resize(N); // TODO: Why is this not in the init code ?
     }
     T *y_row_accumulator = y_row_accumulator_storage.data();
 
@@ -404,17 +414,17 @@ void TCSR_unrolled_tiled(T *X, const CompressedTCSR &W, T *b, T *Y, int M, int N
                 {
                     for (int u = 0; u < UNROLL_W_NNZ; ++u)
                     {
-                    #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                         flops++;
-                    #endif
+#endif
                         y_row_accumulator[encoded_cols[nnz_ptr_W + u]] += x_mk_val;
                     }
                 }
                 for (; nnz_ptr_W < W_k_pos_entries_end_offset; ++nnz_ptr_W)
                 {
-                #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                     flops++;
-                #endif
+#endif
                     y_row_accumulator[encoded_cols[nnz_ptr_W]] += x_mk_val;
                 }
 
@@ -423,17 +433,17 @@ void TCSR_unrolled_tiled(T *X, const CompressedTCSR &W, T *b, T *Y, int M, int N
                 {
                     for (int u = 0; u < UNROLL_W_NNZ; ++u)
                     {
-                    #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                         flops++;
-                    #endif
+#endif
                         y_row_accumulator[-encoded_cols[nnz_ptr_W + u] - 1] -= x_mk_val;
                     }
                 }
                 for (; nnz_ptr_W < W_k_row_end_offset; ++nnz_ptr_W)
                 {
-                #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                     flops++;
-                #endif
+#endif
                     y_row_accumulator[-encoded_cols[nnz_ptr_W] - 1] -= x_mk_val;
                 }
             }
@@ -470,22 +480,22 @@ void TCSC(T *X, const CompressedTCSC &W, T *b, T *Y, int M, int N, int K)
 
             for (int k = col_start; k < col_start + pos_count; k++)
             {
-            #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                 flops++;
-            #endif
+#endif
                 y += X_row[encoded_rows[k]];
             }
 
             for (int k = col_start + pos_count; k < col_offsets[n + 1]; k++)
             {
-            #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                 flops++;
-            #endif
+#endif
                 y -= X_row[-encoded_rows[k] - 1];
             }
-        #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
             flops++;
-        #endif
+#endif
             Y_row[n] = y + b[n];
         }
     }
@@ -529,25 +539,28 @@ void TCSC_unrolled_tiled(T *X, const CompressedTCSC &W, T *b, T *Y, int M, int N
                         T partials[UNROLL_K] = {0};
                         for (; k + UNROLL_K <= pos_end; k += UNROLL_K)
                         {
-                            for (int u = 0; u < UNROLL_K; ++u) {
+                            for (int u = 0; u < UNROLL_K; ++u)
+                            {
                                 partials[u] += X_row[encoded_rows[k + u]];
-                            #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                                 flops++;
-                            #endif        
+#endif
                             }
                         }
-                        for (int u = 0; u < UNROLL_K; ++u) {
+                        for (int u = 0; u < UNROLL_K; ++u)
+                        {
                             acc += partials[u];
-                        #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                             flops++;
-                        #endif
+#endif
                         }
                     }
-                    for (; k < pos_end; ++k) {
+                    for (; k < pos_end; ++k)
+                    {
                         acc += X_row[encoded_rows[k]];
-                    #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                         flops++;
-                    #endif
+#endif
                     }
 
                     k = pos_end;
@@ -556,32 +569,34 @@ void TCSC_unrolled_tiled(T *X, const CompressedTCSC &W, T *b, T *Y, int M, int N
                         T partials[UNROLL_K] = {0};
                         for (; k + UNROLL_K <= col_end; k += UNROLL_K)
                         {
-                            for (int u = 0; u < UNROLL_K; ++u) {
+                            for (int u = 0; u < UNROLL_K; ++u)
+                            {
                                 partials[u] -= X_row[-encoded_rows[k + u] - 1];
-                            #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                                 flops++;
-                            #endif        
+#endif
                             }
                         }
-                        for (int u = 0; u < UNROLL_K; ++u) {
+                        for (int u = 0; u < UNROLL_K; ++u)
+                        {
                             acc += partials[u];
-                        #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                             flops++;
-                        #endif    
+#endif
                         }
                     }
-                    for (; k < col_end; ++k) {
+                    for (; k < col_end; ++k)
+                    {
                         acc -= X_row[-encoded_rows[k] - 1];
-                    #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                         flops++;
-                    #endif
+#endif
                     }
                     Y_row[n] = acc;
                 }
             }
         }
     }
-
 }
 
 template <typename T, int UNROLL_FACTOR>
@@ -611,26 +626,26 @@ void BaseCSC_unr(T *X, const BaseTCSC &W_csc, T *b, T *Y, int M, int N, int K)
                 for (int u = 0; u < UNROLL_FACTOR; u++)
                 {
                     y_pos[u] += X[m * K + row_index_pos[k_pos_loop + u]];
-                #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                     flops++;
-                #endif
+#endif
                 }
             }
 
             T y_pos_final = 0;
             for (int u = 0; u < UNROLL_FACTOR; u++)
             {
-            #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                 flops++;
-            #endif
+#endif
                 y_pos_final += y_pos[u];
             }
 
             for (; k_pos_loop < end_pos; k_pos_loop++)
             {
-            #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                 flops++;
-            #endif
+#endif
                 y_pos_final += X[m * K + row_index_pos[k_pos_loop]];
             }
 
@@ -641,9 +656,9 @@ void BaseCSC_unr(T *X, const BaseTCSC &W_csc, T *b, T *Y, int M, int N, int K)
             {
                 for (int u = 0; u < UNROLL_FACTOR; u++)
                 {
-                #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                     flops++;
-                #endif
+#endif
                     y_neg[u] += X[m * K + row_index_neg[k_neg_loop + u]];
                 }
             }
@@ -651,22 +666,22 @@ void BaseCSC_unr(T *X, const BaseTCSC &W_csc, T *b, T *Y, int M, int N, int K)
             T y_neg_final = 0;
             for (int u = 0; u < UNROLL_FACTOR; u++)
             {
-            #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                 flops++;
-            #endif
+#endif
                 y_neg_final += y_neg[u];
             }
 
             for (; k_neg_loop < end_neg; k_neg_loop++)
             {
-            #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                 flops++;
-            #endif
+#endif
                 y_neg_final += X[m * K + row_index_neg[k_neg_loop]];
             }
-        #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
             flops += 2;
-        #endif
+#endif
             Y[m * N + n] = (y_pos_final - y_neg_final) + b[n];
         }
     }
@@ -702,18 +717,18 @@ void BaseCSR(T *X, const BaseTCSR &W_csr, T *b, T *Y, int M, int N, int K)
 
             for (int j = row_start_pos[k]; j < row_start_pos[k + 1]; ++j)
             {
-            #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                 flops++;
-            #endif
+#endif
                 int n_col = col_index_pos[j];
                 Y[m * N + n_col] += x_val;
             }
 
             for (int j = row_start_neg[k]; j < row_start_neg[k + 1]; ++j)
             {
-            #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                 flops++;
-            #endif
+#endif
                 int n_col = col_index_neg[j];
                 Y[m * N + n_col] -= x_val;
             }
@@ -756,18 +771,18 @@ void BaseCSR_unr(T *X, const BaseTCSR &W_csr, T *b, T *Y, int M, int N, int K)
             {
                 for (int u = 0; u < UNROLL_FACTOR; ++u)
                 {
-                #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                     flops++;
-                #endif
+#endif
                     int n_col = col_index_pos[j_pos + u];
                     Y[m * N + n_col] += x_val;
                 }
             }
             for (; j_pos < end_pos; ++j_pos)
             {
-            #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                 flops++;
-            #endif
+#endif
                 int n_col = col_index_pos[j_pos];
                 Y[m * N + n_col] += x_val;
             }
@@ -779,18 +794,18 @@ void BaseCSR_unr(T *X, const BaseTCSR &W_csr, T *b, T *Y, int M, int N, int K)
             {
                 for (int u = 0; u < UNROLL_FACTOR; ++u)
                 {
-                #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                     flops++;
-                #endif
+#endif
                     int n_col = col_index_neg[j_neg + u];
                     Y[m * N + n_col] -= x_val;
                 }
             }
             for (; j_neg < end_neg; ++j_neg)
             {
-            #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                 flops++;
-            #endif
+#endif
                 int n_col = col_index_neg[j_neg];
                 Y[m * N + n_col] -= x_val;
             }
@@ -899,9 +914,9 @@ template void TCSC_unrolled_tiled<float, 12, 8, 8>(float *, const TCSCMatrix &, 
                     {
                         for (int u = 0; u < UNROLL_FACTOR; u++)
                         {
-                        #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                             flops++;
-                        #endif
+#endif
                             y_pos[u] += X[m * K + row_index_pos[k_pos_loop + u]];
                         }
                     }
@@ -909,17 +924,17 @@ template void TCSC_unrolled_tiled<float, 12, 8, 8>(float *, const TCSCMatrix &, 
                     T y_pos_final = 0;
                     for (int u = 0; u < UNROLL_FACTOR; u++)
                     {
-                    #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                         flops++;
-                    #endif
+#endif
                         y_pos_final += y_pos[u];
                     }
 
                     for (; k_pos_loop < end_pos; k_pos_loop++)
                     {
-                    #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                         flops++;
-                    #endif
+#endif
                         y_pos_final += X[m * K + row_index_pos[k_pos_loop]];
                     }
 
@@ -930,9 +945,9 @@ template void TCSC_unrolled_tiled<float, 12, 8, 8>(float *, const TCSCMatrix &, 
                     {
                         for (int u = 0; u < UNROLL_FACTOR; u++)
                         {
-                        #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                             flops++;
-                        #endif
+#endif
                             y_neg[u] += X[m * K + row_index_neg[k_neg_loop + u]];
                         }
                     }
@@ -940,22 +955,22 @@ template void TCSC_unrolled_tiled<float, 12, 8, 8>(float *, const TCSCMatrix &, 
                     T y_neg_final = 0;
                     for (int u = 0; u < UNROLL_FACTOR; u++)
                     {
-                    #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                         flops++;
-                    #endif
+#endif
                         y_neg_final += y_neg[u];
                     }
 
                     for (; k_neg_loop < end_neg; k_neg_loop++)
                     {
-                    #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                         flops++;
-                    #endif
+#endif
                         y_neg_final += X[m * K + row_index_neg[k_neg_loop]];
                     }
-                #ifdef INSTRUMENTATION_RUN
+#ifdef INSTRUMENTATION_RUN
                     flops += 2;
-                #endif
+#endif
                     Y[m * N + n] = (y_pos_final - y_neg_final) + b[n];
                 }
             }
